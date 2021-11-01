353eb97cb9dd5c13b059daf0865788cd3f7f8291
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-11-01 09:48:49 +0100

    Initial commit on branch

--- index.js
+++ index.js
@@ -1,95 +1,32 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-const tabBtn = document.getElementById("tab-btn");
+class Leads{
+  constructor() {
+    this.getLeads = this.getLeads.bind(this);
+    this.saveLead = this.saveLead.bind(this);
+    this.removeLead = this.removeLead.bind(this);
+    this.clearLeads = this.clearLeads.bind(this);
+  };
 
-//Add item from the input when saveInput button is clicked.
-inputBtn.addEventListener("click", function () {
-  myLeads.push({id: Date.now(), value: inputEl.value});
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
-});
-
-//Checking if there is item in our array variable.
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
+getLeads() {
+ return JSON.parse(localStorage.getItem('leads')) ?? [];
 }
 
-//Add the url of the current or active tabs to our array variable when saveTab button is clicked.
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-});
+saveLead(lead) {
+  const leads = this.getLeads();
+  leads.push(lead);
+  localStorage.setItem('leads', JSON.stringify(leads));
+}
 
-//Render url as a list link
-function render(leads) {
-  let listItems = "";
-  for (let i = 0; i < leads.length; i++) {
-    if (
-      leads[i].value.includes("@", 0) ||
-      leads[i].value.includes(".", 0) ||
-      leads[i].value.includes("www.", 0) ||
-      leads[i].value.includes("https", 0) ||
-      leads[i].value.includes("http", 0) ||
-      leads[i].value.includes("/", 0) ||
-      leads[i].value.includes(":", 0) ||
-      leads[i].value.includes("|", 0)
-    ) {
-      listItems += `
-      <li>
-      <button data-index=${leads[i].id} class="deleteItemBtn">
-          <i class="fas fa-trash-alt" id="delete-item"></i>
-          </button>
-          <a target='_blank' href='${leads[i].value}'>
-              ${leads[i].value}
-          </a>
-      </li>
-  `;
-    } else {
-      listItems += `
-      <li>
-      <button data-index=${leads[i].id}  class="deleteItemBtn">
-          <i class="fas fa-trash-alt" id="delete-item"></i>
-          </button>
-              ${leads[i].value} 
-      </li>
-  `;
-    }
+  removeLead(leadId) {
+    // console.log('Lead removed...', leadId);
+    const newLeads = this.getLeads().filter(lead => lead.id !== leadId);
+    localStorage.setItem('leads', JSON.stringify(newLeads));
   }
-  ulEl.innerHTML = listItems;
-    }
 
-  //Delete individual items
-// deleteItem.addEventListener ("click", function () {
-// const deleteItem = (i) => {
-const delBtn = document.querySelectorAll(".deleteItemBtn");
-delBtn.forEach(btn => btn.addEventListener("click", (e) => {
-  const index = e.target.closest("li");
-  console.log(index)
-    let leads = localStorage.getItem("myLeads");
-    leads = JSON.parse(leads);
-    // let currentLeads = leads.filter(item => item.id =! e.target.dataset.index);
-    // console.log(e.target.dataset.index)
-    ulEl.removeChild(index);
-    localStorage.setItem("myLeads", JSON.stringify(currentLeads));
-    // render(currentLeads);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  // render(myLeads);
-}));
+  clearLeads() {
+    localStorage.removeItem('leads');
+    console.log('Lead cleared.');
+  }
+}
 
-//Delete all saved url when delete button is double clicked.
-deleteBtn.addEventListener("dblclick", function () {
-  // console.log("double clicked")
-  localStorage.removeItem("myLeads");
-  myLeads = [];
-  ulEl.innerHTML = "";
-  // render();
-});
+var global = window || global;
+global.Leads = Leads
---
+++ ui.js
@@ -0,0 +1,114 @@
+class UI {
+    constructor() {
+      this.initSaveTab = this.initSaveTab.bind(this);
+      this.initForm = this.initForm.bind(this);
+      this.addItem = this.addItem.bind(this);
+      this.updateDomNode = this.updateDomNode.bind(this);
+      this.initRemoveItem = this.initRemoveItem.bind(this);
+      this.initClearAll = this.initClearAll.bind(this);
+    }
+  
+    MyLeads = new Leads();
+  
+    initSaveTab() {
+      const saveTabBtn = document.getElementById('save-tab-btn');
+      //Add the url of the current or active tabs to our array variable when saveTab button is clicked.
+      saveTabBtn.addEventListener('click', () => {
+        // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+        const lead = { id: Date.now(), value: window.location.toString() };
+        this.MyLeads.saveLead(lead);
+        this.addItem(lead);
+        this.initRemoveItem();
+      });
+    }
+  
+    initForm() {
+      console.log('Form init...');
+      const inputValue = document.getElementById('input-el');
+      const saveBtn = document.getElementById('save-btn');
+  
+      saveBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!inputValue.value) return false;
+        const lead = { id: Date.now(), value: inputValue.value.trim() };
+        this.addItem(lead);
+        this.MyLeads.saveLead(lead);
+        this.initRemoveItem();
+      });
+    }
+  
+    addItem(item) {
+      let listItem = '';
+      if (
+        item.value.includes('@', 0) ||
+        item.value.includes('.', 0) ||
+        item.value.includes('www.', 0) ||
+        item.value.includes('https', 0) ||
+        item.value.includes('http', 0) ||
+        item.value.includes('/', 0) ||
+        item.value.includes(':', 0) ||
+        item.value.includes('|', 0)
+      ) {
+        listItem += `
+        <button class="delete-item-btn">
+          <i class="fas fa-trash-alt" class="delete-item-btn"></i>
+            </button>
+            <a target='_blank' href=${item.value}>
+              ${item.value}
+            </a>`;
+      } else {
+        listItem += `
+        <button class="delete-item-btn">
+          <i class="fas fa-trash-alt" class="delete-item-btn"></i></button>
+            ${item.value}`;
+      }
+      let li = document.createElement('li');
+      li.setAttribute('data-id', item.id);
+      li.innerHTML = listItem;
+      document
+        .querySelector('#list-container')
+        .insertAdjacentElement('afterbegin', li);
+    }
+  
+    updateDomNode() {
+      const leads = this.MyLeads.getLeads();
+      console.log(leads);
+      if (!(leads || leads.length)) return false;
+      // Use Document Fragment for render optimization
+      // const frag = new DocumentFragment();
+      leads.map((lead) => this.addItem(lead));
+    }
+  
+    initRemoveItem() {
+      const delBtn = document.querySelectorAll('.delete-item-btn');
+      delBtn.forEach((btn) =>
+        btn.addEventListener('click', (e) => {
+          // console.log('remove item: ', e.target.closest('li'));
+          this.MyLeads.removeLead(parseInt(e.target.closest('li').dataset.id));
+          e.target.closest('li').remove();
+        })
+      );
+    }
+  
+    initClearAll() {
+      const clearBtn = document.querySelector('#clear-btn');
+      clearBtn.addEventListener('click', () => {
+        if (confirm('Are you sure you want to clear all entries?')) {
+          console.log('Clearing all lead entries...');
+          this.MyLeads.clearLeads();
+          document.querySelector('#list-container').innerHTML = '';
+        }
+      });
+    }
+  
+    init() {
+      this.initForm();
+      this.initClearAll();
+      this.updateDomNode();
+      this.initRemoveItem();
+      this.initSaveTab();
+    }
+  }
+  
+  new UI().init();
+  
